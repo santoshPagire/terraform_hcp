68fe37c074c51c0c931b62f6c4114a3b9e507af0
santoshPagire <santoshpagire1007@gmail.com>
2024-10-23 11:29:31 +0530

    provider update

--- providers.tf
+++ providers.tf
@@ -2,4 +2,6 @@ provider "azurerm" {
   features {}
   resource_provider_registrations = "none"
   subscription_id                 = var.subscription_id
+  tenant_id = var.tenant_id
+  use_cli = "false"
 }
--- variables.tf
+++ variables.tf
@@ -2,3 +2,8 @@ variable "subscription_id" {
   type = string
   default = "664b6097-19f2-42a3-be95-a4a6b4069f6b"
 }
+
+variable "tenant_id" {
+    default = "0adb040b-ca22-4ca6-9447-ab7b049a22ff"
+  
+}
